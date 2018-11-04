22f61a22c8c442d20d1e99fa087639476ae5d17e
Mac Chiang <macchiang@gmail.com>
2018-11-04 10:54:51 +0800

    Add add gcp repo

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build . -t react-redux:latest
+docker build . -t react-redux:latest -t gcr.io/my-gcp-221201/react-redux:latest
 docker push gcr.io/my-gcp-221201/react-redux:latest
